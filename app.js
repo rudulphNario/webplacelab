ff68352527a5d8865da4202b3a8f3860d2a1e2fd
rudulphNario <rudulphval@yahoo.com.ph>
2022-12-01 15:35:11 -0800

    second commit

--- app.js
+++ app.js
@@ -23,4 +23,6 @@ app.get("/", function(req, res){
 
 
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(process.env.PORT || 3000, function (req, res) {
+    console.log("Server is running on port 3000");
+  });
